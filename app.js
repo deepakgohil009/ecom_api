703efe745ba1b079280cde5df4a0c5c396dec8ff
Gohil Deepak <93052295+deepakgohil009@users.noreply.github.com>
2022-10-10 20:28:27 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express")
 const mongoose = require("mongoose")
 const dotenv = require("dotenv")
 const cookieParser = require("cookie-parser")
-const cors=require("cors")
+// const cors=require("cors")
 
 const authRoute = require("./routes/auth")
 const productRoute = require("./routes/product")
@@ -27,7 +27,14 @@ const connect = async () => {
 }
 
 //  middlewares
-app.use(cors())
+// app.use(cors())
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
 app.use(express.json())
 app.use(cookieParser())
 app.use("/auth",authRoute)
