7f0124aea6a45e029aac9a269f9e08d4bb087b10
Gohil Deepak <93052295+deepakgohil009@users.noreply.github.com>
2022-10-10 21:05:50 +0530

    Update app.js

--- app.js
+++ app.js
@@ -28,13 +28,6 @@ const connect = async () => {
 
 //  middlewares
 app.use(cors())
-// app.use(function(req, res, next) {
-//     res.setHeader('Access-Control-Allow-Origin', '*');
-//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-//     res.setHeader('Access-Control-Allow-Credentials', true);
-//     next();
-// });
 app.use(express.json())
 app.use(cookieParser())
 app.use("/auth",authRoute)
